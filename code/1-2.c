ba7c58723b9f13bf28da9a8a58426c6a9111cd6a
yosi475 <yosi475@naver.com>
2026-05-08 10:18:18 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -18,8 +18,10 @@ int main() {
     sprintf(s.data, "hello world");
 
     printf("version: %u, size: %zu\n", s.version, sizeof(s.version));
-    prinft("ihl: %u, ize: %zu\n", s.ihl, sizeof(s.ihl));
+    printf("ihl: %u, size: %zu\n", s.ihl, sizeof(s.ihl));
     printf("ttl: %llu, size: %zu\n", s.ttl, sizeof(s.ttl));
     printf("protocol: %llu, size: %zu\n", s.protocol, sizeof(s.protocol));
     printf("data: %s, size: %zu\n", s.data, sizeof(s.data));
+
+    return 0;
 }
